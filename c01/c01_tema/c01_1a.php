65650c2b414c466c77d75e568be7ac0edbbcce60
Mincă Daniel Andrei <andrei_17_09@hotmail.com>
2013-12-21 14:29:37 +0200

    Refreshed file

--- c01/c01_tema/c01_1a.php
+++ c01/c01_tema/c01_1a.php
@@ -1,16 +1,18 @@
 <?php 
 $rows = 10; // numar randuri
 $cols = 2;// numar coloane
-$start = 5; // STARTUL !
 
 echo "<table border='1'>"; 
  
-for($tr = 1 ; $tr <= $rows ; $tr++){      
-    //am intrat in TR 
-    echo "<tr>"; 
-    echo "<td>".($start+5)."</td>"; //sa bag aici un for ??  
-    echo "</tr>";
+for($tr=1;$tr<=$rows;$tr++){      
+     echo "<tr>";
+        for($td=1;$td<=$cols;$td++){
+            //am generat col si randuri, jos trec calculul
+             echo "<td>".($tr*5)."</td>";
+        } 
+    echo "</tr>"; 
 }
  
 echo "</table>"; 
+
 ?>
