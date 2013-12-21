e9b7176a4ccf86ae9664f1b241db7183efc841b5
Mincă Daniel Andrei <andrei_17_09@hotmail.com>
2013-12-21 16:30:59 +0200

    Commited $_SESSION

--- curs08/c08_lucru/03_sesiuni/view_session.php
+++ curs08/c08_lucru/03_sesiuni/view_session.php
@@ -2,6 +2,9 @@
 $t = session_start();
 if($t){
 	echo "Nume sesiune". session_name().", id sesiune:". session_id();
+	if(isset($_SESSION)){
+		print_r($_SESSION);
+	}
 } else {
 	echo "Sesiunea nu este pornita";
 }
