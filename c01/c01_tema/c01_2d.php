12443c9b6f27b0e0a28fd5285f04a4e5541a934a
Mincă Daniel Andrei <andrei_17_09@hotmail.com>
2013-12-24 17:30:22 +0200

    Solved 2d
    Mai trebuie integrat codul in if(isset($_GET['btn']))...

--- c01/c01_tema/c01_2d.php
+++ c01/c01_tema/c01_2d.php
@@ -6,11 +6,44 @@
  <body>
   <fieldset>
   	<form method="get" action="">
-  		<label>Selectati imaginea:</label><br/>
-  		<?php
-  		# generam <option value cu cod PHP...
-  		
+  		<label>Selectati imaginea:</label>
+      <?php      
+      if (isset($_GET['btn'])) { // daca avem selectie
+        //preluam selectia
+
+      } else { // daca nu, generam eroare
+
+      }
+      #------------TABLE-------------
+      $imgs = array('img/browser1.jpg', 'img/browser2.jpg', 'img/browser3.jpg', 'img/browser4.jpg', 'img/browser5.jpg');
+
+    		echo "<table border='1'>"; // open Table
+         
+        for($tr=1;$tr<= 1;$tr++){      
+             echo "<tr>"; // deschid TR
+
+                for($td=1;$td<= count($imgs);$td++){ // deschid TD
+                  echo "<td>";
+                  for ($i=1; $i <= count($imgs); $i++) { 
+                    echo "<img src=\"img/browser$i.jpg\" alt=\"poza[$i]\">";
+                  }
+                }
+            echo "</tr>"; // inchid TR
+        }
+         
+        echo "</table>";  // inchid table & bye!
+        #end code
+
+      #----------OPTION LIST----------
+        echo "<select>";
+        for ($i=0; $i < count($imgs); $i++) { 
+                echo "<option value=\"cols\">Nr coloane: ".$i."</option>";
+              }
+        echo "</select>";
+
   		?>
+
+      <input type="submit" name="btn" value="Trigger haxX!"/>
   	</form>
   </fieldset>
  </body>
