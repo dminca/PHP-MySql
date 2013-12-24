e2940614073755e868362c75d957dcf12ee96979
Mincă Daniel Andrei <andrei_17_09@hotmail.com>
2013-12-24 11:20:27 +0200

    Solved Issue #3
    Exe 03 done.

---
+++ c01/c01_tema/c01_2b.php
@@ -0,0 +1,21 @@
+<?php
+$cols = 2;
+$rows = 4;
+$nr = 5;
+echo "<table border='1'>"; // open Table
+ 
+for($tr=1;$tr<=$rows;$tr++){      
+     echo "<tr>"; // deschid TR
+
+        for($td=1;$td<=$cols;$td++){ // deschid TD
+        	echo "<td>";
+        	for ($i=1; $i <= $nr; $i++) { 
+        		echo "<img src=\"img/browser$i.jpg\" alt=\"poza[$i]\">";
+        	}
+        }
+    echo "</tr>"; // inchid TR
+}
+ 
+echo "</table>";  // inchid table & bye!
+#end code
+?>
---
+++ c01/c01_tema/c01_2c.php
@@ -0,0 +1,8 @@
+<?php
+include('lib/functions.php'); # added table function
+
+tabel_imagini(2, 4, 5);
+# 2 rows
+# 4 cols
+# 5 total images
+?>
