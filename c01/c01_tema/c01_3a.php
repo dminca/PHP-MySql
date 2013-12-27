0f8ba94900209c368c20132723fe885cc2bf629b
Mincă Daniel Andrei <andrei_17_09@hotmail.com>
2013-12-27 02:10:18 +0200

    Issue #3 is done

--- c01/c01_tema/c01_3a.php
+++ c01/c01_tema/c01_3a.php
@@ -1,16 +1,20 @@
 <?php
-$cols = 5;
-$go = 1;
+$cols = 5; // coloane
+$box = 10; // cutii
+$go = 0;
 
 echo "<table border='1'>"; // open Table
  
-for($tr=1;$tr<= 1;$tr++){      
+for($tr=1;$tr<= 2;$tr++){ // generate 2 rows   
      echo "<tr>"; // deschid TR
 
-        for($td=1;$td<=$cols;$td++){ // deschid TD
+        for($td=1;$td<=$cols;$td++){ // deschid 5 td-uri
         	echo "<td>";
-        	for ($i=1; $i <= $cols; $i++) { 
-        		echo ($go++)."x";
+			$go++;  // start 'go'
+			
+			// in td I'm doing the operation
+        	for ($i=1; $i <= $box; $i++) { 
+        		echo $go."x"."$i"."=" . ($go*$i) . "<br/>"; // the core: Operation calculates here 
         	}
         	echo "</td>";
         }
