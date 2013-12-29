47f06169a92dea38ee54335d865b34f2db99bd7e
Mincă Daniel Andrei <andrei_17_09@hotmail.com>
2013-12-29 15:42:52 +0200

    Trying to integrate cmmdc calculation in $columns

--- c01/c01_tema/lib/inmultire.php
+++ c01/c01_tema/lib/inmultire.php
@@ -1,19 +1,28 @@
 <?php
+
+/*
+------------------------------------------------
+----------------[ TABEL v. 1]-------------------
+------------------------------------------------
+*/
 function inmultire($cells, $columns)
 {
     $go = 0;
 
     echo "<table border='1'>"; // open Table
 
-    for ($tr = 1; $tr <= 2; $tr++) { // generate 2 rows
+    for ($tr = 1; $tr <= 2; $tr++)
+    { // generate 2 rows
         echo "<tr>"; // deschid TR
 
-        for ($td = 1; $td <= $columns; $td++) { // Open <td>
+        for ($td = 1; $td <= $columns; $td++)
+        { // Open <td>
             echo "<td>";
             $go++; // start 'go'
 
             // in td I'm doing the operation
-            for ($i = 1; $i <= $cells; $i++) {
+            for ($i = 1; $i <= $cells; $i++)
+            {
                 echo $go . "x" . $i . "=" . ($go * $i) . "<br/>"; // the core: Operation calculates here
             }
             echo "</td>";
@@ -24,4 +33,61 @@ function inmultire($cells, $columns)
     echo "</table>"; // closing <table> and BYE!
 }
 #end code
+
+
+/*
+------------------------------------------------
+----------------[ TABEL v. 2]-------------------
+------------------------------------------------
+*/
+function tabel($cells, $columns)
+{
+    $go = 0;
+
+    echo "<table border='1'>"; // open Table
+
+    for ($tr = 1; $tr <= 2; $tr++)
+    { // generate 2 rows
+        echo "<tr>"; // deschid TR
+
+        for ($td = 1; $td <= $columns; $td++)
+        { // Open <td>
+            echo "<td>";
+            $go++; // start 'go'
+
+            // in td I'm doing the operation
+            for ($i = 1; $i <= $cells; $i++)
+            {
+                echo $go . "x" . $i . "=" . ($go * $i) . "<br/>"; // the core: Operation calculates here
+            }
+            echo "</td>";
+        }
+        echo "</tr>"; // closing <TR>
+    }
+
+    echo "</table>"; // closing <table> and BYE!
+}
+#end code
+
+
+/*
+------------------------------------------------
+----------------[ C.M.M.D.C.]-------------------
+------------------------------------------------
+*/
+function cmmdc($a, $b)
+{
+    while ($a != $b)
+    {
+        if ($a > $b)
+        {
+            $a -= $b;
+        } else
+        {
+            $b -= $a;
+        }
+    }
+    echo $a; // this is the cmmdc output
+}
+
 ?>
